eba0a45a4a390e56a3e248747036e849221ac208
maksudul087 <sm.mimon96@gmail.com>
2017-06-09 23:26:27 +0600

    Detailed page populated with api data

---
+++ imports/ui/models/DetailedPageContent.jsx
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {createContainer} from 'meteor/react-meteor-data';
+
+class DetailedPageContent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: '',
+        };
+    }
+    change(){
+        this.setState({
+            error: '',
+        });
+    }
+    renderError(){
+        if(this.state.error != ''){
+            return(
+                <div className="ui negative message">
+                    <div className="header">Errors</div>
+                    <p>{this.state.error}</p>
+                </div>
+            );
+        }
+    }
+    handleSubmit(event){
+
+        event.preventDefault();
+        let username = ReactDOM.findDOMNode(this.refs.username).value.trim();
+        let password = ReactDOM.findDOMNode(this.refs.password).value.trim();
+
+        Meteor.loginWithPassword(username, password, (error, result) => {
+            if(error){
+                console.log(error);
+                this.setState({
+                    error: "Error: Username and password don't match",
+                });
+            }
+            else{
+
+                    FlowRouter.go('/dashboard');
+
+
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className='loginSectionCon'>
+
+                <form className="" onSubmit={this.handleSubmit.bind(this)}>
+                    <span>Login</span>
+                    <div className='logFieldContainer'>
+                        Username
+                        <input ref="username" type="text" className="loginField" placeholder="Username" onChange={this.change.bind(this)}/>
+                        <br/>
+                        Password
+                        <input ref="password" type="password" className="loginField" placeholder="Password" onChange={this.change.bind(this)}/>
+                        <br/>
+                        {this.renderError()}
+                        <div className='two ui buttons'>
+                            <button className="ui blue button newColor">Login</button>
+                        </div>
+                    </div>
+                    <a href="/signup">Create an Account</a>
+                </form>
+
+            </div>
+        );
+    }
+}
+export default createContainer( (props) => {
+    return{
+        user: Meteor.user(),
+    };
+},DetailedPageContent );
